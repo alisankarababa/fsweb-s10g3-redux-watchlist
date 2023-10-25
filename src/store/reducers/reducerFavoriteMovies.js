1f847cb8bf4bb66e7ebd7c28fd72587fc0280e0d
Alişan Karababa <alisankarababa@gmail.com>
2023-10-25 13:20:16 +0300

    bir film bir kez favorilere eklenebilsin diye güncelleme

--- src/store/reducers/reducerFavoriteMovies.js
+++ src/store/reducers/reducerFavoriteMovies.js
@@ -9,7 +9,10 @@ export default function reducerFavoriteMovies(state=initialState, action) {
 
     switch (action.type) {
         case actionsFavoriteMovies.MOVIE_ADDED:
-            
+
+            if ( state.movies.find((movie) => movie.id === action.payload.id) )
+                return state;
+
             return {
                 ...state,
                 movies: [...state.movies, action.payload]
