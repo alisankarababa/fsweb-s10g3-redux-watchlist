ea5a7544c5557f99710a3b41c22626027c297d35
Alişan Karababa <alisankarababa@gmail.com>
2023-10-25 13:07:36 +0300

    redux logger setup

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
-import { legacy_createStore as createStore } from 'redux'
-
+import { legacy_createStore as createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 import { reducers } from "./reducers/index"
 
-export const store = createStore(reducers);
+
+const middleware = applyMiddleware(logger);
+
+export const store = createStore(reducers, middleware);
